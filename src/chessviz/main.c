4b91aff66c43a0eb5bd9eab056737b827257500c
Bogdan Gerasimov <Mackkei@localhost.localdomain>
2021-03-19 23:37:20 +0700

    Add of work cycle

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,10 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <chessviz/board_read.h>
+#include <libchessviz/libchessbiz.a>
 
 int main()
 {
     char desk[9][9];
     char move[11];
+
+    board(desk);
+    boardPrint(desk);
+
+    while (true) {
+        printf("\nType a move: ");
+        boardRead(move);
+        move(desk, move);
+        if (move(desk, move) == 1)
+            break;
+        boardPrint(desk);
+    }
 
     return 0;
 }

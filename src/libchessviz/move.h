b5388464d570ea89430fc65c189753ac77d94cd9
Герасимов Богдан Дмитриевич <ip012s07@bwp11.csc.local>
2021-04-17 13:04:56 +0700

    enum macros add for tests

--- src/libchessviz/move.h
+++ src/libchessviz/move.h
@@ -1,3 +1,16 @@
 #pragma once
 
+enum test {
+	WRONGINPUT = 1,
+	EXITSUCCESS,
+	PAWNERROR,
+	KINGERROR,
+	BORDERLIMIT,
+	QUEENERROR,
+	ROOKERROR,
+	KNIGHTERROR,
+	BISHOPERROR,
+	CHECKMATE;
+	};
+	
 int move(char desk[][9], char chessMove[]);
